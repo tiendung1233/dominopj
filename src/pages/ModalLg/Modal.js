adb0f8f5cdba97747436f0fb5956aa732220dbac
tiendung1233 <nguyentiendung159357@gmail.com>
2022-06-04 21:03:42 +0700

    voucher

--- src/pages/ModalLg/Modal.js
+++ src/pages/ModalLg/Modal.js
@@ -4,9 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Link } from 'react-router-dom'
 import Styles from './ModalLg.module.css'
+import imgLg1 from './imgLg/dmlimglgsm.png'
 export default function ModalLg() {
     const [modal, setModal] = useState(false);
-
+    const toggleLogin = () =>{
+        console.log( document.getElementsByClassName('box_lg12'))
+       document.getElementsByClassName('box_lg12').classList.add('.display_none')  
+              
+    }
+    const toggleRegin = () =>{
+        document.getElementsByClassName('box_regin1k2s').classList.add('display_block')  
+        document.getElementsByClassName('box_lg12').classList.add('display_none')  
+            
+     }
     const toggleModal = () => {
         setModal(!modal);
     };
@@ -27,26 +37,54 @@ export default function ModalLg() {
                 <div className={"modal"}>
                     <div onClick={toggleModal} className="overlay"></div>
                     <div className={Styles.modal_content}>
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" />
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="row">
-                                                <div class="col-md-6">
-                                                    Đăng nhập
-                                                </div>
-                                                <div class="col-md-6">
-                                                    Tạo tài khoản
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div >
+                            <img src={imgLg1} className={Styles.img_Lg_sm} />
+                        </div>
+                        <div className={`${Styles.box_lg} box_lg12`}>
+                            <div className={Styles.btn_login}>
+                                    <div className={`${Styles.btn_login1} g12343` } onClick ={toggleLogin}>Đăng nhập</div>
+                                   <div className={`${Styles.btn_login2} h123j` } onClick={toggleRegin}>Tạo tài khoản</div>
                             </div>
+                            <div className={Styles.Email_text}> Email</div>
+                            <div className={Styles.email}>
+                                <input className={Styles.email_ip} typeof="email" placeholder="Nhập tên đăng nhập" />
+                            </div>
+                            <div className={Styles.pass_text}>Mật khẩu</div>
+                            <div className={Styles.pass}>
+
+                                <input className={Styles.pass_ip} typeof="password" placeholder="Nhập mật khẩu" />
+                            </div>
+                            <button className={Styles.ss}>Đăng nhập</button>
+                        </div>
+
+                        <div className={`${Styles.box_regin} box_regin1k2`}>
+                            <div className={Styles.btn_regin}>
+                            <div className={`${Styles.btn_login1} g12343` } onClick ={toggleLogin}>Đăng nhập</div>
+                                   <div className={`${Styles.btn_login2} h123j` } onClick={toggleRegin}>Tạo tài khoản</div>
+                            </div>
+                            <div className={Styles.name_text}> Họ và tên</div>
+                            <div className={Styles.name}>
+                                <input className={Styles.name_ip} placeholder="Nhập họ và tên của bạn" />
+                            </div>
+                            <div className={Styles.number_text}>Số điện thoại</div>
+                            <div className={Styles.number}>
+                                <input className={Styles.number_ip}  placeholder="Nhập số điện thoại của bạn" />
+                            </div>
+                            <div className={Styles.number_text}>Số điện thoại</div>
+                            <div className={Styles.number}>
+                                <input className={Styles.number_ip}  placeholder="Nhập số điện thoại của bạn" />
+                            </div>
+                            <div className={Styles.number_text}>Số điện thoại</div>
+                            <div className={Styles.number}>
+                                <input className={Styles.number_ip}  placeholder="Nhập số điện thoại của bạn" />
+                            </div>
+                            <div className={Styles.number_text}>Số điện thoại</div>
+                            <div className={Styles.number}>
+                                <input className={Styles.number_ip}  placeholder="Nhập số điện thoại của bạn" />
+                            </div>
+
+                            <button className={Styles.btn_regin_ss}>Đăng kí</button>
+
                         </div>
                     </div>
                 </div>
--- src/pages/Voucher/Voucher.js
+++ src/pages/Voucher/Voucher.js
@@ -1,5 +1,133 @@
+import Styles from './Voucher.module.css'
 function Voucher() {
-    return ( <div>Voucher pages</div> );
+    return (
+        <div className={Styles.Voucher_page}>
+            <div className={Styles.Voucher_conten}>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class={`col-md-3 ${Styles.img_style}`}>
+                                    <img alt="Bootstrap Image Preview" className='img_width' src="https://img.dominos.vn/Banner+Promotion_COMBO+T%E1%BA%B6NG+%C4%90%E1%BB%96+MI+N%C3%94-04.png" />
+                                </div>
+                                <div className={`col-md-9 ${Styles.tab_voucher}`}>
+                                    <h3 className={Styles.title}>
+                                        COMBO XỈU CHỒNG XỈU ĐỔ TIẾT KIỆM ĐẾN 30% & TẶNG ĐỖ MI NÔ LEO NÚI
+
+                                    </h3>
+                                    <p className={Styles.conten1w}>
+                                        <p>
+                                            * Combo Ngon Xỉu Chồng 299K (2-3 người): 1 Pizza Lạp Xưởng Xốt Trứng Muối Mayonnaise cỡ vừa, 1 Pizza Favorite cỡ vừa, 3 thức uống nhỏ & Tặng 1 Đỗ Mi Nô Leo Núi phiên bản giới hạn.
+
+                                        </p>
+                                        <p>
+                                            * Combo Vui Xỉu Đổ 449K (4-5 người): 1 Pizza Lạp Xưởng Xốt Trứng Muối Mayonnaise cỡ vừa, 1 Pizza Favorite cỡ vừa, Xúc xích xông khói (8pcs)/ Cánh gà BBQ kiểu Mỹ (4pcs), 1 Choco Pizza, 4 thức uống nhỏ & Tặng 1 Đỗ Mi Nô Leo Núi phiên bản giới hạn. </p>
+
+                                    </p>
+                                    <div className={Styles.btn_voucher}>
+                                        <button className={Styles.btn_voucher_item}> Ngon Xỉu Chồng 299K </button>
+                                        <button className={Styles.btn_voucher_item}> Vui Xỉu Đổ 449k </button>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class={`col-md-3 ${Styles.img_style}`}>
+                                    <img alt="Bootstrap Image Preview" className='img_width' src="https://img.dominos.vn/Promotion-70-pizza-thu-2.png" />
+                                </div>
+                                <div className={`col-md-9 ${Styles.tab_voucher}`}>
+                                    <h3 className={Styles.title}>
+                                        GIẢM 70% CHO PIZZA THỨ 2 - ÁP DỤNG MUA MANG VỀ & GIAO HÀNG TẬN NƠI
+
+                                    </h3>
+                                    <p className={Styles.conten1w}>
+                                        <p>
+                                            * Mua Pizza size M hoặc L kèm nước uống nguyên giá được giảm 70% cho bánh Pizza thứ 2 cùng size có giá bằng hoặc thấp hơn Pizza thứ nhất.
+
+                                        </p>
+                                        <p>
+                                            * Áp dụng cho Mua Mang Về & Giao Hàng Tận Nơi tất cả các ngày trong tuần.
+
+                                        </p>
+                                    </p>
+                                    <div className={Styles.btn_voucher}>
+                                        <button className={Styles.btn_voucher_item}> Đặt Size M </button>
+                                        <button className={Styles.btn_voucher_item}> Đặt Size L </button>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class={`col-md-3 ${Styles.img_style}`}>
+                                    <img alt="Bootstrap Image Preview" className='' src="https://img.dominos.vn/Promotion%2BVN-EN-1.png" />
+                                </div>
+                                <div className={`col-md-9 ${Styles.tab_voucher}`}>
+                                    <h3 className={Styles.title}>
+                                        MIX-MATCH - TIẾT KIỆM 45%, PIZZA CHỈ TỪ 89K
+                                    </h3>
+                                    <p className={Styles.conten1w}>
+                                        <p>
+                                            * Giá: 89K/pizza dòng Favorite (size M). Thêm 30K/pizza và 60K/pizza để nâng lên dòng Premium và Signature.
+
+                                        </p>
+                                        <p>
+                                            * Giá: 159K/pizza dòng Favorite (size L). Thêm 20K/pizza và 60K/pizza để nâng lên dòng Premium và Signature.
+
+                                        </p>
+                                        <p>
+                                            * Áp dụng khi mua 02 pizza đồng thời.</p>
+
+                                    </p> 
+                                    <div className={Styles.btn_voucher}>
+                                        <button className={Styles.btn_voucher_item}> 02 Favorite (M) </button>
+                                        <button className={Styles.btn_voucher_item}> 02 Premium (M)  </button>
+                                        <button className={Styles.btn_voucher_item}> 02 Signature (M) </button>
+                                        <button className={Styles.btn_voucher_item}>01 Favorite (M) + 01 Premium (M)</button>
+                                        <button className={Styles.btn_voucher_item}> 01 Signature (M) + 01 Premium (M) </button>
+                                        <button className={Styles.btn_voucher_item}> 02 Favorite (L) </button>
+                                        <button className={Styles.btn_voucher_item}> 01 Signature (L) + 01 Premium (L) </button>
+                                        <button className={Styles.btn_voucher_item}>  02 Signature (L)</button>
+                                        <button className={Styles.btn_voucher_item}> 01 Premium (L) + 01 Favorite (L) </button>
+                                        <button className={Styles.btn_voucher_item}> 01 Signature (L) + 01 Favorite (L) </button>
+                                        <button className={Styles.btn_voucher_item}> 01 Signature (M) + 01 Favorite (M) </button>
+                                        <button className={Styles.btn_voucher_item}> 02 Premium (L) </button>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class={`col-md-3 ${Styles.img_style}`}>
+                                    <img alt="Bootstrap Image Preview" className='img_width' src="https://img.dominos.vn/Promotion+Deal+cho+nhom+VN_EN-1.png" />
+                                </div>
+                                <div className={`col-md-9 ${Styles.tab_voucher}`}>
+                                    <h3 className={Styles.title}>
+                                        DEAL SIÊU TO CHO NHÓM</h3>
+                                    <p className={Styles.conten1w}>
+                                        <p>
+                                            * Combo 229K cho 2 người gồm 1 pizza size M dòng Premium + 1 phần món phụ + 1 chai nước (390ml)
+
+                                        </p>
+                                        <p>
+                                            * Combo 309K cho 3-4 người gồm 1 pizza size L dòng Favorite + 2 phần món phụ + 2 chai nước (390ml)
+
+                                        </p>
+                                        <p>
+                                            * Combo 339K cho 3-4 người gồm 1 pizza size L dòng Premium + 2 phần món phụ + 1 chai nước (1.5L)</p>
+
+                                    </p>
+                                    <div className={Styles.btn_voucher}>
+                                        <button className={Styles.btn_voucher_item}> Combo 229K</button>
+                                        <button className={Styles.btn_voucher_item}> Combo 309K </button>
+                                        <button className={Styles.btn_voucher_item}> Combo 339K </button>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>);
 }
 
 export default Voucher;
