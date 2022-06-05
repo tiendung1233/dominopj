41986aa20a727a63295011569541b7f45e36d912
tiendung1233 <nguyentiendung159357@gmail.com>
2022-06-05 17:41:28 +0700

    mdlg

--- src/pages/Follow/Follow.js
+++ src/pages/Follow/Follow.js
@@ -1,5 +1,31 @@
+import Styles from './Follow.module.css'
 function Follow() {
-    return ( <div>Follow pages</div> );
+    return (
+        <div className={Styles.Follow}>
+            <div className={Styles.conten}>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-5">
+                                    <img alt="Bootstrap Image Preview" className={Styles.img_width} src="https://dominos.vn/img/stock-stracking.png" />
+                                </div>
+                                <div class="col-md-7 mt-4">
+                                    <h3 className={Styles.conten_text}>
+                                        Kiểm tra đơn hàng của bạn
+                                    </h3>
+                                    <div className={Styles.box_ip}>
+                                        <input className={Styles.input} placeholder='Nhập só điện thoại của bạn'/>
+                                         <butto className={Styles.box_ipbtn}><i className={`bi bi-search ${Styles.icon_s}`}></i></butto>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>);
 }
 
 export default Follow;
--- src/pages/ModalLg/Modal.js
+++ src/pages/ModalLg/Modal.js
@@ -7,23 +7,27 @@ import Styles from './ModalLg.module.css'
 import imgLg1 from './imgLg/dmlimglgsm.png'
 export default function ModalLg() {
     const [modal, setModal] = useState(false);
-    const toggleLogin = () =>{
-        console.log( document.getElementsByClassName('box_lg12'))
-       document.getElementsByClassName('box_lg12').classList.add('.display_none')  
-              
+    const [modalOpen, setModalOpen] = useState('')
+
+    if(modalOpen =='register'){
+        console.log(1)
+        document.querySelector('.form_login').classList.add('display_none')
+        document.querySelector('.form_regin').classList.remove('display_none')
     }
-    const toggleRegin = () =>{
-        document.getElementsByClassName('box_regin1k2s').classList.add('display_block')  
-        document.getElementsByClassName('box_lg12').classList.add('display_none')  
-            
-     }
+    if(modalOpen =='login'){
+        console.log(2)
+        document.querySelector('.form_regin').classList.add('display_none')
+        document.querySelector('.form_login').classList.remove('display_none')
+
+
+    }
     const toggleModal = () => {
         setModal(!modal);
     };
 
     if (modal) {
         document.body.classList.add('active-modal')
-
+        
     } else {
 
         document.body.classList.remove('active-modal')
@@ -40,50 +44,59 @@ export default function ModalLg() {
                         <div >
                             <img src={imgLg1} className={Styles.img_Lg_sm} />
                         </div>
-                        <div className={`${Styles.box_lg} box_lg12`}>
+                        <div className={`${Styles.box_lg} form_login`}>
                             <div className={Styles.btn_login}>
-                                    <div className={`${Styles.btn_login1} g12343` } onClick ={toggleLogin}>Đăng nhập</div>
-                                   <div className={`${Styles.btn_login2} h123j` } onClick={toggleRegin}>Tạo tài khoản</div>
-                            </div>
-                            <div className={Styles.Email_text}> Email</div>
-                            <div className={Styles.email}>
-                                <input className={Styles.email_ip} typeof="email" placeholder="Nhập tên đăng nhập" />
-                            </div>
-                            <div className={Styles.pass_text}>Mật khẩu</div>
-                            <div className={Styles.pass}>
+                                <div className={`${Styles.btn_login1} g12343`}>
 
-                                <input className={Styles.pass_ip} typeof="password" placeholder="Nhập mật khẩu" />
+                                    Đăng nhập</div>
+                                <div className={`${Styles.btn_login2} h123j`} 
+                                onClick={() => {
+                                    setModalOpen('register')
+                                }}>Tạo tài khoản</div>
                             </div>
-                            <button className={Styles.ss}>Đăng nhập</button>
+                            <div className="">
+                                <div className={Styles.Email_text}> Email</div>
+                                <div className={Styles.email}>
+                                    <input className={Styles.email_ip} typeof="email" placeholder="Nhập tên đăng nhập" />
+                                </div>
+                                <div className={Styles.pass_text}>Mật khẩu</div>
+                                <div className={Styles.pass}>
+
+                                    <input className={Styles.pass_ip} typeof="password" placeholder="Nhập mật khẩu" />
+                                </div>
+                                <button className={Styles.ss}>Đăng nhập</button>
+                            </div>
                         </div>
 
-                        <div className={`${Styles.box_regin} box_regin1k2`}>
+                        <div className={`${Styles.box_regin} form_regin display_none`}>
                             <div className={Styles.btn_regin}>
-                            <div className={`${Styles.btn_login1} g12343` } onClick ={toggleLogin}>Đăng nhập</div>
-                                   <div className={`${Styles.btn_login2} h123j` } onClick={toggleRegin}>Tạo tài khoản</div>
+                                <div className={`${Styles.btn_login1} g12343`} onClick={() => { setModalOpen('login') }}>Đăng nhập</div>
+                                <div className={`${Styles.btn_login2} h123j`} >Tạo tài khoản</div>
                             </div>
-                            <div className={Styles.name_text}> Họ và tên</div>
-                            <div className={Styles.name}>
-                                <input className={Styles.name_ip} placeholder="Nhập họ và tên của bạn" />
-                            </div>
-                            <div className={Styles.number_text}>Số điện thoại</div>
-                            <div className={Styles.number}>
-                                <input className={Styles.number_ip}  placeholder="Nhập số điện thoại của bạn" />
-                            </div>
-                            <div className={Styles.number_text}>Số điện thoại</div>
-                            <div className={Styles.number}>
-                                <input className={Styles.number_ip}  placeholder="Nhập số điện thoại của bạn" />
-                            </div>
-                            <div className={Styles.number_text}>Số điện thoại</div>
-                            <div className={Styles.number}>
-                                <input className={Styles.number_ip}  placeholder="Nhập số điện thoại của bạn" />
-                            </div>
-                            <div className={Styles.number_text}>Số điện thoại</div>
-                            <div className={Styles.number}>
-                                <input className={Styles.number_ip}  placeholder="Nhập số điện thoại của bạn" />
-                            </div>
+                            <div className=" ">
+                                <div className={Styles.name_text}> Họ và tên</div>
+                                <div className={Styles.name}>
+                                    <input className={Styles.name_ip} placeholder="Nhập họ và tên của bạn" />
+                                </div>
+                                <div className={Styles.number_text}>Số điện thoại</div>
+                                <div className={Styles.number}>
+                                    <input className={Styles.number_ip} placeholder="Nhập số điện thoại của bạn" />
+                                </div>
+                                <div className={Styles.number_text}>Số điện thoại</div>
+                                <div className={Styles.number}>
+                                    <input className={Styles.number_ip} placeholder="Nhập số điện thoại của bạn" />
+                                </div>
+                                <div className={Styles.number_text}>Số điện thoại</div>
+                                <div className={Styles.number}>
+                                    <input className={Styles.number_ip} placeholder="Nhập số điện thoại của bạn" />
+                                </div>
+                                <div className={Styles.number_text}>Số điện thoại</div>
+                                <div className={Styles.number}>
+                                    <input className={Styles.number_ip} placeholder="Nhập số điện thoại của bạn" />
+                                </div>
 
-                            <button className={Styles.btn_regin_ss}>Đăng kí</button>
+                                <button className={Styles.btn_regin_ss}>Đăng kí</button>
+                            </div>
 
                         </div>
                     </div>
